b7aaba2114f8c195a511e38f716450e1d2be2c54
Ian Shakespeare <ian.shakespeare@dmail.dixie.edu>
2021-11-23 09:34:21 -0700

    continued work on block emulation

--- myfs.c
+++ myfs.c
@@ -7,28 +7,38 @@ struct SuperBlock {
     uint64_t inodes;
 };
 
+
+/* BLOCK INTERACTION */
 int opendisk(char *filename, uint64_t size) {
-    int fd = open("/mnt/e/CS/3400/fs/v.disk", O_RDWR);
-    if (fd < 0) {
-        fd = open("/mnt/e/CS/3400/fs/v.disk", O_RDWR | O_CREAT);
-    }
-
-
-    bad:
-        return -1;
+    int fd = open("./v.disk", O_RDWR | O_CREAT);
+    ftruncate(fd, size);
+    return fd;
 }
 
 int readblock(int handle, uint64_t blocknum, void *buffer) {
-    
+    lseek(handle, blocknum * 4096, SEEK_SET);
+    int r = read(handle, buffer, 4096);
+    lseek(handle, 0, SEEK_SET);
+    return r;
 }
-int writeblock(int handle, uint64_t blocknum, void *buffer) {
 
+int writeblock(int handle, uint64_t blocknum, void *buffer) {
+    lseek(handle, blocknum * 4096, SEEK_SET);
+    int w = write(handle, buffer, 4096);
+    lseek(handle, 0, SEEK_SET);
+    return w;
 }
 
 int syncdisk(int handle) {
-
+    int f = fsync(handle);
+    return f;
 }
 
 int closedisk(int handle) {
+    int c = close();
+    return c;
+}
 
+int formatdisk(int handle) {
+    struct SuperBlock sb = { 0xDEADBEEF, 512, 0 };
 }
